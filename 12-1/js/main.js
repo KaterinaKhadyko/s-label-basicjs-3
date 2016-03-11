f4bb81b9ddac49128dfb2697a56999f16f527ab5
ievhenganchin@gmail.com <334023MNUsh!>
2016-03-11 21:44:23 +0200

    Shop (updates)

--- 12-1/js/main.js
+++ 12-1/js/main.js
@@ -1,20 +1,65 @@
-
 /*scroll to top*/
+
+var productsTemplate = {
+	source: "templates/products.html",
+	$element: $(".features_items"),
+	data: {},
+	hanlebarsTpl: ""
+};
 
 $(document).ready(function(){
 	$(function () {
+		var productsTemplate = null;
 
-		$.ajax("templates/categories.html").done(function (templateSource) {
-			var template = Handlebars.compile(templateSource);
+		function renderProducts(data) {
+			if (productsTemplate) {
+				$(".features_items").html(productsTemplate(data));
+				return;
+			}
 
-			$.ajax("data/init.json").done(function(data) {
+			$.ajax("templates/products.html").done(function (templateSource) {
+						productsTemplate = Handlebars.compile(templateSource);
+						$(".features_items").html(productsTemplate(data));
+					});
+		}
+
+		$.ajax("data/init.json").done(function(data) {
+			$.ajax("templates/categories.html").done(function (templateSource) {
+				var template = Handlebars.compile(templateSource);
 				$(".category-products").html(template(data));
+				$(".category-products h4.panel-title a").on("click", function(e) {
+					e.preventDefault();
+					var categoryId = +$(this).closest("[data-id]").attr("data-id");
+					var productsData = {
+						products: []
+					};
+
+					$.each(data.products, function(id, product) {
+						if (product.categories.indexOf(categoryId) != -1) {
+							productsData.products.push(product);
+						}
+					});
+
+					renderProducts(productsData);
+				});
 			});
+
+			$.ajax("templates/price-range.html").done(function (templateSource) {
+				var template = Handlebars.compile(templateSource);
+
+				var priceData = {
+					min: 0,
+					max: 1000,
+					from: 600,
+					to: 800
+				};
+
+				$(".price-range").html(template(priceData));
+				$('#sl2').slider();
+			});
+
+			renderProducts(data);
 		});
-
-		/*price range*/
-
-		$('#sl2').slider();
 
 		var RGBChange = function() {
 			$('#RGB').css('background', 'rgb('+r.getValue()+','+g.getValue()+','+b.getValue()+')')
